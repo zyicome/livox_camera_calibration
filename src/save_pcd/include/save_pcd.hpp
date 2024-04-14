e05b90dc0f97f6d5bc7ad273c5cf2826b6e19785
snappython <2677785349@qq.com>
2024-04-14 21:15:16 +0800

    init

---
+++ src/save_pcd/include/save_pcd.hpp
@@ -0,0 +1,36 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include "cv_bridge/cv_bridge.h"
+#include <iostream>
+#include <rclcpp/rclcpp.hpp>
+#include <pcl/point_cloud.h>
+#include <sensor_msgs/msg/detail/image__struct.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <sensor_msgs/msg/compressed_image.hpp>
+#include <pcl/conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <vector>
+
+#include <pcl/visualization/pcl_visualizer.h>
+
+#include <fstream>
+
+#include <boost/thread/thread.hpp>
+
+class RadarDeal : public rclcpp::Node
+{
+public:
+    RadarDeal();
+
+    void pointCloudCallback(const sensor_msgs::msg::PointCloud2 &input);
+
+    // publisher
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr cloud_pub;;
+    // subscriber
+    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr cloud_sub;
+};
---
+++ src/save_pcd/src/save_pcd.cpp
@@ -0,0 +1,71 @@
+#include "save_pcd.hpp"
+
+#include "pcl/filters/passthrough.h"
+
+RadarDeal::RadarDeal() : Node("radar_deal")
+{
+    cloud_pub = this->create_publisher<sensor_msgs::msg::PointCloud2>("/livox/deal", 10);
+
+    cloud_sub = this->create_subscription<sensor_msgs::msg::PointCloud2>("/livox/lidar", 10, std::bind(&RadarDeal::pointCloudCallback, this, std::placeholders::_1));
+}
+
+void RadarDeal::pointCloudCallback(const sensor_msgs::msg::PointCloud2 &input)
+{
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PCLPointCloud2 pcl_pc2;
+    pcl_conversions::toPCL(input, pcl_pc2);
+    pcl::fromPCLPointCloud2(pcl_pc2, *cloud);   
+    
+    /*pcl::PassThrough<pcl::PointXYZ> pass;
+    pass.setInputCloud(cloud);
+    pass.setFilterFieldName("z");
+    pass.setFilterLimits(0, -250);
+    pass.setFilterLimitsNegative(true);
+    pass.filter(*cloud);*/
+
+    /*pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
+    tree->setInputCloud(cloud);
+    std::vector<pcl::PointIndices> cluster_indices;
+    pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
+    ec.setClusterTolerance(0.5);
+    ec.setMinClusterSize(100);
+    ec.setMaxClusterSize(25000);
+    ec.setSearchMethod(tree);
+    ec.setInputCloud(cloud);
+    ec.extract(cluster_indices);
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster_all(new pcl::PointCloud<pcl::PointXYZ>);
+    for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin(); it != cluster_indices.end(); ++it)
+    {
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster(new pcl::PointCloud<pcl::PointXYZ>);
+        for (std::vector<int>::const_iterator pit = it->indices.begin(); pit != it->indices.end(); ++pit)
+        {
+            cloud_cluster->points.push_back(cloud->points[*pit]);
+        }
+        cloud_cluster->width = cloud_cluster->points.size();
+        cloud_cluster->height = 1;
+        cloud_cluster->is_dense = true;
+
+        *cloud_cluster_all += *cloud_cluster;
+    }*/
+    int i = 0;
+
+    for(i= 0;i<200;i++)
+    {
+        pcl::io::savePCDFileASCII("/home/mechax/zyb/lidar_camera_calibration_data/calibration_pcd.pcd", *cloud);
+        std::cout << "success to save pcd!" << std::endl;
+    }
+
+    /*sensor_msgs::msg::PointCloud2 output;
+    pcl::toROSMsg(*cloud_cluster_all, output);
+    output.header.frame_id = "livox_frame";
+    cloud_pub->publish(output);*/
+}
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<RadarDeal>());
+    rclcpp::shutdown();
+    return 0;
+}
